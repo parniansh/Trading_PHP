b5867b3b916ba66cfb8158ea69baa762ab87aab3
shirdast <parnian.shirdast@gmail.com>
2022-05-30 13:14:26 +0430

    second commit

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -14,6 +14,7 @@ class LoginController extends Controller
     //
 
     public function login(Request $request){
+        
 
         $request -> validate(
             ['email' => ['required','email'],
